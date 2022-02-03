5c314f05e9345e6896de7819c134a8d3b4306cd5
mkroeders <mkroeders@gmail.com>
2022-02-03 23:02:31 +0100

    Added day/week/month/year

---
+++ test/Number/Int/Date/WeekTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace LessValueObjectTest\Number\Int\Date;
+
+use LessValueObject\Number\Int\Date\Week;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \LessValueObject\Number\Int\Date\Week
+ */
+final class WeekTest extends TestCase
+{
+    public function testRange(): void
+    {
+        self::assertSame(1, Week::getMinValue());
+        self::assertSame(53, Week::getMaxValue());
+    }
+}
