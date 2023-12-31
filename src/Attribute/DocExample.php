ee37ad3d40700ee591153e54ea9b604718c765c3
mkroeders <mkroeders@gmail.com>
2023-12-31 11:12:00 +0100

    Only allow string examples

--- src/Attribute/DocExample.php
+++ src/Attribute/DocExample.php
@@ -8,6 +8,6 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 final class DocExample
 {
-    public function __construct(public readonly mixed $example)
+    public function __construct(public readonly string $example)
     {}
 }
