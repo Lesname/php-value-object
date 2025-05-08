d89ad0a96fbd4387ae3434a91ba984c474a6acab
mkroeders <mkroeders@gmail.com>
2025-05-08 23:16:09 +0200

    Added signature composite value objects

---
+++ src/Composite/Signature/SignatureCompositeValueObject.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace LesValueObject\Composite\Signature;
+
+use Countable;
+use Traversable;
+use LesValueObject\ValueObject;
+use LesValueObject\Composite\CompositeValueObject;
+use LesValueObject\Composite\Signature\Exception\PropertyNotFound;
+
+/**
+ * @template T of ValueObject
+ *
+ * @extends Traversable<T>
+ *
+ * @psalm-immutable
+ */
+interface SignatureCompositeValueObject extends CompositeValueObject, Countable, Traversable
+{
+    /**
+     * @throws PropertyNotFound
+     *
+     * @return T
+     */
+    public function get(string $key): ValueObject;
+
+    public function has(string $key): bool;
+
+    /**
+     * @return class-string<T>
+     *
+     * @psalm-pure
+     */
+    public static function getSignature(): string;
+}
